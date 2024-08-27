25cdab9fb87c7e9160a11228be5b73570f928b29
Renato Gouveia <rpgouveia@gmail.com>
2024-08-27 13:48:41 -0300

    refactor: add resize process to image_segmentation function

--- core/image_manipulation.py
+++ core/image_manipulation.py
@@ -5,18 +5,33 @@ import cv2 as cv
 import os
 
 
+def image_resize(image, input_size: int):
+    # Calculate image proportion for resize operation
+    height, width = image.shape[:2]
+    aspect_ratio = width / height
+    new_height = input_size
+    new_width = int(input_size * aspect_ratio)
+
+    # Resize image
+    resized_img = cv.resize(image, (new_width, new_height))
+    return resized_img
+
+
 def image_segmentation(filename, base_mask_folder, output_folder):
     img_number = filename.split(".")[0]
     mask_path: str = os.path.join(base_mask_folder, f"{img_number}.png")
 
     # Load and Convert Images
-    bgr_img = cv.imread(mask_path, 1)
-    hsv_img = cv.cvtColor(bgr_img, cv.COLOR_BGR2HSV)
+    bgr_img: ndarray = cv.imread(mask_path, 1)
+    hsv_img: ndarray = cv.cvtColor(bgr_img, cv.COLOR_BGR2HSV)
+
+    # Resize process
+    resized_hsv_img = image_resize(hsv_img, input_size=512)
 
     # Create masks for each color and concatenate them
     masks: list[ndarray] = []
     for lower_bound, upper_bound in color_limits:
-        mask: ndarray = cv.inRange(hsv_img, lower_bound, upper_bound)
+        mask: ndarray = cv.inRange(resized_hsv_img, lower_bound, upper_bound)
         # Add new axis for each mask binary mask
         mask = mask[..., np.newaxis]
         masks.append(mask)
