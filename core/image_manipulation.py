721cf5f3ccd7e9f37642d552e2988666ba4d6f2a
Renato <rpgouveia@gmail.com>
2025-02-05 20:52:08 -0300

    refactor: update image_manipulation.py

--- core/image_manipulation.py
+++ core/image_manipulation.py
@@ -16,15 +16,6 @@ def image_resize(image: ndarray, input_size: int) -> ndarray:
     Returns:
         The resized image as a NumPy array with the new dimensions.
     """
-    # Modificação: Remoção do cálculo de proporção para viabilizar formato 256, 256, 18
-    # Calculate image proportion for resize operation
-    # height, width = image.shape[:2]
-    # aspect_ratio = width / height
-    # new_height = input_size
-    # new_width = int(input_size * aspect_ratio)
-
-    # Resize image
-    # resized_img: ndarray = cv.resize(image, (new_width, new_height))
     resized_img: ndarray = cv.resize(image, (input_size, input_size))
     return resized_img
 
@@ -32,6 +23,7 @@ def image_resize(image: ndarray, input_size: int) -> ndarray:
 def image_segmentation(filename: str, base_mask_folder: str, output_folder: str) -> None:
     """
     Segments an image based on predefined color limits and saves the resulting masks as a NumPy array.
+    The masks are resized after segmentation.
 
     Parameters:
         filename (str): The name of the image file to be segmented, including its extension (e.g., "image1.jpg").
@@ -41,10 +33,10 @@ def image_segmentation(filename: str, base_mask_folder: str, output_folder: str)
     Procedure:
         1. Extracts the base name of the image (without extension) to locate the corresponding mask file.
         2. Loads the mask image and converts it from BGR to HSV color space.
-        3. Resizes the HSV image to a specified height while maintaining its aspect ratio.
-        4. Applies a series of color-based masks to the resized HSV image.
-        5. Concatenates all masks along the channel dimension to form a combined mask.
-        6. Saves the combined mask as a NumPy file (.npy) in the specified output folder.
+        3. Applies a series of color-based masks to the HSV image.
+        4. Concatenates all masks along the channel dimension.
+        5. Resizes the combined mask to the specified dimensions.
+        6. Saves the resized combined mask as a NumPy file (.npy) in the specified output folder.
 
     Returns:
         None. The function saves the combined mask directly to the output folder.
@@ -56,14 +48,10 @@ def image_segmentation(filename: str, base_mask_folder: str, output_folder: str)
     bgr_img: ndarray = cv.imread(mask_path, 1)
     hsv_img: ndarray = cv.cvtColor(bgr_img, cv.COLOR_BGR2HSV)
 
-    # Resize process
-    # Modificação: Redimensionar máscara no formato 256,256,18
-    resized_hsv_img = image_resize(hsv_img, input_size=256)
-
     # Create masks for each color and concatenate them
     masks: list[ndarray] = []
     for lower_bound, upper_bound in color_limits:
-        mask: ndarray = cv.inRange(resized_hsv_img, lower_bound, upper_bound)
+        mask: ndarray = cv.inRange(hsv_img, lower_bound, upper_bound)
         # Add new axis for each mask binary mask
         mask = mask[..., np.newaxis]
         masks.append(mask)
@@ -71,6 +59,9 @@ def image_segmentation(filename: str, base_mask_folder: str, output_folder: str)
     # Concatenate masks along the channel dimension Z
     combined_mask: ndarray = np.concatenate(masks, axis=2)
 
-    # Save combined mask as a NumPy file
+    # Resize the combined mask after segmentation
+    resized_combined_mask = image_resize(combined_mask, input_size=256)
+
+    # Save resized combined mask as a NumPy file
     output_file: str = os.path.join(output_folder, f"{img_number}.npy")
-    np.save(output_file, combined_mask)
+    np.save(output_file, resized_combined_mask)
