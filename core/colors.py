ad05e74992e49780c1a29665973970f5e64a9d5c
Renato Gouveia <rpgouveia@gmail.com>
2025-03-14 16:57:09 -0300

    refactor: update colors.py, update image_manipulation.py

--- core/colors.py
+++ core/colors.py
@@ -1,72 +1,297 @@
+from typing import Any
 import numpy as np
 
 
-# The color limits in HSV space color for all 22 classes
-# lower_bound = np.array(H, S, V) and upper_bound = np.array(H, S, V)
-color_limits = [
+# The class parameters with detailed configuration for all 22 classes
+class_parameters: list[dict[str, Any]] = [
     # Building (#85ff0c): Building
-    (np.array([40, 49, 210]), np.array([44, 194, 255])),
+    {
+        "color_limits": (np.array([40, 49, 210]), np.array([44, 194, 255])),
+        "min_area": 50,
+        "kernel_size": (2, 2),
+        "growth_conditions": {
+            "h_type": "normal",
+            "h_range": (38, 46),
+            "s_range": (45, 200),
+            "v_range": (205, 255)
+        }
+    },
     
     # Mobility Devices (#137f49): Motorized Wheelchair
-    (np.array([77, 132, 185]), np.array([79, 151, 188])),
+    {
+        "color_limits": (np.array([77, 132, 185]), np.array([79, 151, 188])),
+        "min_area": 5,
+        "kernel_size": (2, 2),
+        "growth_conditions": {
+            "h_type": "normal",
+            "h_range": (75, 81),
+            "s_range": (130, 153),
+            "v_range": (183, 190)
+        }
+    },
 
     # Mobility Devices (#13647f): Crutch
-    (np.array([92, 132, 183]), np.array([98, 151, 188])),
+    {
+        "color_limits": (np.array([92, 132, 183]), np.array([98, 151, 188])),
+        "min_area": 5,
+        "kernel_size": (4, 4),
+        "growth_conditions": {
+            "h_type": "circular",   # Wraparound condition
+            "h_range": (151, 18),
+            "s_range": (72, 193),
+            "v_range": (166, 251)
+        }
+    },
     
     # Mobility Devices (#64137f): Walker
-    (np.array([141, 150, 187]), np.array([145, 173, 221])),
+    {
+        "color_limits": (np.array([141, 150, 187]), np.array([145, 173, 221])),
+        "min_area": 5,
+        "kernel_size": (2, 2),
+        "growth_conditions": {
+            "h_type": "normal",
+            "h_range": (141, 147),
+            "s_range": (140, 183),
+            "v_range": (177, 231)
+        }
+    },
 
     # Mobility Devices (#7f7f13): Wheelchair
-    (np.array([20, 140, 178]), np.array([31, 151, 198])),
+    {
+        "color_limits": (np.array([20, 140, 178]), np.array([31, 151, 198])),
+        "min_area": 5,
+        "kernel_size": (2, 2),
+        "growth_conditions": {
+            "h_type": "normal",
+            "h_range": (18, 33),
+            "s_range": (138, 153),
+            "v_range": (176, 200)
+        }
+    },
 
     # Mobility Devices (#13137f): Orthopedic Cane
-    (np.array([120, 150, 187]), np.array([129, 163, 205])),
+    {
+        "color_limits": (np.array([120, 150, 187]), np.array([129, 163, 205])),
+        "min_area": 5,
+        "kernel_size": (2, 2),
+        "growth_conditions": {
+            "h_type": "normal",
+            "h_range": (118, 131),
+            "s_range": (148, 165),
+            "v_range": (185, 210)
+        }
+    },
 
     # Mobility Devices (#2e7f13): Cane
-    (np.array([32, 77, 163]), np.array([72, 163, 214])),
+    {
+        "color_limits": (np.array([32, 77, 163]), np.array([72, 163, 214])),
+        "min_area": 5,
+        "kernel_size": (2, 2),
+        "growth_conditions": {
+            "h_type": "normal",
+            "h_range": (30, 74),
+            "s_range": (75, 165),
+            "v_range": (160, 220)
+        }
+    },
 
     # Mobility Devices (#7f1349): Orthopedic Crutch
-    (np.array([161, 149, 187]), np.array([168, 178, 212])),
+    {
+        "color_limits": (np.array([161, 149, 187]), np.array([168, 178, 212])),
+        "min_area": 5,
+        "kernel_size": (2, 2),
+        "growth_conditions": {
+            "h_type": "normal",
+            "h_range": (159, 170),
+            "s_range": (147, 180),
+            "v_range": (185, 215)
+        }
+    },
 
     # Nature (#0cffff): Grass
-    (np.array([90, 192, 255]), np.array([90, 194, 255])),
+    {
+        "color_limits": (np.array([90, 192, 255]), np.array([90, 194, 255])),
+        "min_area": 30,
+        "kernel_size": (2, 2),
+        "growth_conditions": {
+            "h_type": "normal",
+            "h_range": (88, 92),
+            "s_range": (190, 196),
+            "v_range": (250, 255)
+        }
+    },
 
     # Nature (#0cff49): Tree, Plants
-    (np.array([73, 192, 255]), np.array([73, 194, 255])),
+    {
+        "color_limits": (np.array([73, 192, 255]), np.array([73, 194, 255])),
+        "min_area": 30,
+        "kernel_size": (2, 2),
+        "growth_conditions": {
+            "h_type": "normal",
+            "h_range": (71, 75),
+            "s_range": (190, 196),
+            "v_range": (250, 255)
+        }
+    },
 
     # Passerby (#ff0c0c): Humans
-    (np.array([0, 230, 240]), np.array([176, 255, 255])),
+    {
+        "color_limits": (np.array([0, 230, 240]), np.array([176, 255, 255])),
+        "min_area": 20,
+        "kernel_size": (2, 2),
+        "growth_conditions": {
+            "h_type": "normal",
+            "h_range": (0, 176),
+            "s_range": (225, 255),
+            "v_range": (235, 255)
+        }
+    },
 
     # Passerby (#4c2f13): Dogs
-    (np.array([15, 191, 76]), np.array([15, 191, 76])),
+    {
+        "color_limits": (np.array([15, 191, 76]), np.array([15, 191, 76])),
+        "min_area": 5,
+        "kernel_size": (2, 2),
+        "growth_conditions": {
+            "h_type": "normal",
+            "h_range": (14, 16),
+            "s_range": (189, 193),
+            "v_range": (74, 78)
+        }
+    },
 
     # Street Furniture (#ff0cc2): Boullard, Signpost, Bench, Public Trash Can, Swing, Parasol, Advertising Panel
-    (np.array([154, 193, 255]), np.array([155, 194, 255])),
+    {
+        "color_limits": (np.array([154, 193, 255]), np.array([155, 194, 255])),
+        "min_area": 10,
+        "kernel_size": (2, 2),
+        "growth_conditions": {
+            "h_type": "normal",
+            "h_range": (153, 156),
+            "s_range": (191, 196),
+            "v_range": (250, 255)
+        }
+    },
 
     # Street Furniture (#21134c): Fountain, Monuments, Tourist Spots
-    (np.array([129, 121, 148]), np.array([130, 123, 149])),
+    {
+        "color_limits": (np.array([129, 121, 148]), np.array([130, 123, 149])),
+        "min_area": 10,
+        "kernel_size": (2, 2),
+        "growth_conditions": {
+            "h_type": "normal",
+            "h_range": (128, 131),
+            "s_range": (119, 125),
+            "v_range": (146, 151)
+        }
+    },
 
     # Transport (#ffc20c): Car, Bus, Vehicles
-    (np.array([25, 192, 255]), np.array([26, 194, 255])),
+    {
+        "color_limits": (np.array([25, 192, 255]), np.array([26, 194, 255])),
+        "min_area": 50,
+        "kernel_size": (2, 2),
+        "growth_conditions": {
+            "h_type": "normal",
+            "h_range": (24, 27),
+            "s_range": (190, 196),
+            "v_range": (250, 255)
+        }
+    },
 
     # Transport (#132f4c): Bike
-    (np.array([102, 121, 148]), np.array([103, 123, 149])),
+    {
+        "color_limits": (np.array([102, 121, 148]), np.array([103, 123, 149])),
+        "min_area": 10,
+        "kernel_size": (2, 2),
+        "growth_conditions": {
+            "h_type": "normal",
+            "h_range": (101, 104),
+            "s_range": (119, 125),
+            "v_range": (146, 151)
+        }
+    },
 
     # Transport (#3e4c13): Motorcycle, Scooter
-    (np.array([35, 121, 148]), np.array([36, 123, 149])),
+    {
+        "color_limits": (np.array([35, 121, 148]), np.array([36, 123, 149])),
+        "min_area": 15,
+        "kernel_size": (2, 2),
+        "growth_conditions": {
+            "h_type": "normal",
+            "h_range": (34, 37),
+            "s_range": (119, 125),
+            "v_range": (146, 151)
+        }
+    },
 
     # Urban infrastructure (#7f2f13): Street Light Pole
-    (np.array([0, 102, 187]), np.array([24, 157, 188])),
+    {
+        "color_limits": (np.array([0, 102, 187]), np.array([24, 157, 188])),
+        "min_area": 5,
+        "kernel_size": (2, 2),
+        "growth_conditions": {
+            "h_type": "normal",
+            "h_range": (0, 26),
+            "s_range": (100, 160),
+            "v_range": (185, 190)
+        }
+    },
 
     # Urban infrastructure (#1349ff): Streets
-    (np.array([107, 193, 255]), np.array([107, 194, 255])),
+    {
+        "color_limits": (np.array([107, 193, 255]), np.array([107, 194, 255])),
+        "min_area": 50,
+        "kernel_size": (2, 2),
+        "growth_conditions": {
+            "h_type": "normal",
+            "h_range": (105, 109),
+            "s_range": (191, 196),
+            "v_range": (250, 255)
+        }
+    },
 
     # Urban infrastructure (#134c3e): Speed Sign, Time Limit Parking Sign
-    (np.array([82, 117, 148]), np.array([85, 123, 152])),
+    {
+        "color_limits": (np.array([82, 117, 148]), np.array([85, 123, 152])),
+        "min_area": 5,
+        "kernel_size": (2, 2),
+        "growth_conditions": {
+            "h_type": "normal",
+            "h_range": (80, 87),
+            "s_range": (115, 125),
+            "v_range": (146, 155)
+        }
+    },
 
     # Urban infrastructure (#134c13): Traffic Light Pole
-    (np.array([59, 122, 148]), np.array([60, 123, 150])),
+    {
+        "color_limits": (np.array([59, 122, 148]), np.array([60, 123, 150])),
+        "min_area": 5,
+        "kernel_size": (2, 2),
+        "growth_conditions": {
+            "h_type": "normal",
+            "h_range": (58, 61),
+            "s_range": (120, 125),
+            "v_range": (146, 152)
+        }
+    },
 
     # Urban infrastructure (#8513ff): Sidewalks
-    (np.array([140, 193, 255]), np.array([140, 194, 255]))
+    {
+        "color_limits": (np.array([140, 193, 255]), np.array([140, 194, 255])),
+        "min_area": 50,
+        "kernel_size": (2, 2),
+        "growth_conditions": {
+            "h_type": "normal",
+            "h_range": (138, 142),
+            "s_range": (191, 196),
+            "v_range": (250, 255)
+        }
+    }
 ]
+
+
+# The list of color limits in HSV space color for all 22 classes
+# lower_bound = np.array(H, S, V), upper_bound = np.array(H, S, V)
+color_limits :list = [params["color_limits"] for params in class_parameters]
--- core/image_manipulation.py
+++ core/image_manipulation.py
@@ -1,8 +1,9 @@
-from core.colors import color_limits
+from core.colors import class_parameters
 from numpy import ndarray
 import numpy as np
 import cv2 as cv
 import os
+import collections
 
 
 def image_resize(image: ndarray, input_size: int) -> ndarray:
@@ -20,15 +21,220 @@ def image_resize(image: ndarray, input_size: int) -> ndarray:
     Returns:
         ndarray: The resized image as a NumPy array with the new dimensions (input_size x input_size).
     """
-
-    resized_img: ndarray = cv.resize(image, (input_size, input_size), interpolation=cv.INTER_AREA)
+    resized_img: ndarray = cv.resize(
+        image, (input_size, input_size), interpolation=cv.INTER_AREA
+    )
     return resized_img
 
 
-def image_segmentation(filename: str, base_mask_folder: str, output_folder: str) -> None:
+def extract_color_mask(
+    hsv_img: ndarray, lower_bound: ndarray, upper_bound: ndarray
+) -> ndarray:
     """
-    Segments an image based on predefined color limits and saves the resulting masks as a NumPy array.
-    The masks are resized after segmentation.
+    Extracts a color mask from an HSV image using specified color bounds.
+
+    Parameters:
+        hsv_img (ndarray): Input image in HSV color space.
+        lower_bound (ndarray): Lower bound of the color range in HSV.
+        upper_bound (ndarray): Upper bound of the color range in HSV.
+
+    Returns:
+        ndarray: Binary mask where pixels within the color range are 255, others are 0.
+    """
+    return cv.inRange(hsv_img, lower_bound, upper_bound)
+
+
+def filter_contours(mask: ndarray, min_area: int = 0) -> ndarray:
+    """
+    Filters contours in a binary mask based on their area.
+
+    Parameters:
+        mask (ndarray): Binary input mask.
+        min_area (int, optional): Minimum contour area to keep. Defaults to 0.
+
+    Returns:
+        ndarray: Filtered binary mask with contours of area >= min_area.
+    """
+    contours, _ = cv.findContours(mask, cv.RETR_TREE, cv.CHAIN_APPROX_SIMPLE)
+
+    # Create empty mask
+    filtered_mask = np.zeros_like(mask)
+
+    # Filter by area and preserve structure
+    for contour in contours:
+        area = cv.contourArea(contour)
+        if area >= min_area:
+            # Create mask for this specific contour
+            temp_mask = np.zeros_like(mask)
+            cv.drawContours(temp_mask, [contour], -1, (255), -1)
+
+            # Preserve original pixels within outline
+            contour_region = cv.bitwise_and(mask, temp_mask)
+            filtered_mask = cv.bitwise_or(filtered_mask, contour_region)
+
+    return filtered_mask
+
+
+def apply_region_growing(
+    filtered_mask: ndarray, hsv_img: ndarray, growth_conditions: dict
+) -> ndarray:
+    """
+    Applies region growing algorithm to a binary mask based on HSV color conditions.
+    Supports both normal ranges and circular ranges for the H component.
+
+    Parameters:
+        filtered_mask (ndarray): Initial binary mask for starting the region growing.
+        hsv_img (ndarray): Input image in HSV color space.
+        growth_conditions (dict): Dictionary with h_type, h_range, s_range, v_range defining the
+                                acceptable ranges for region growing.
+
+    Returns:
+        ndarray: Binary mask after region growing.
+    """
+    # A region growing mask is created. Initially, it contains the extraction
+    region_mask = np.zeros_like(filtered_mask, dtype=np.uint8)
+    region_mask[filtered_mask == 255] = 255
+
+    # Queue for region growing
+    queue = collections.deque()
+
+    # Add initial pixels from extraction
+    indexes = np.column_stack(np.where(filtered_mask == 255))
+    for index in indexes:
+        queue.append(tuple(index))
+
+    # Unpack growth conditions
+    h_type = growth_conditions["h_type"]
+    h_range = growth_conditions["h_range"]
+    s_range = growth_conditions["s_range"]
+    v_range = growth_conditions["v_range"]
+
+    # Define boundary condition for region growing, with support for circular H ranges
+    def fulfill_condition(H, S, V):
+        # Check S and V ranges (always normal ranges)
+        s_condition = s_range[0] <= S <= s_range[1]
+        v_condition = v_range[0] <= V <= v_range[1]
+
+        # Check H range depending on the type
+        if h_type == "normal":
+            # Normal continuous range
+            h_condition = h_range[0] <= H <= h_range[1]
+        elif h_type == "circular":
+            # Circular range that crosses 0/180
+            if h_range[0] > h_range[1]:  # Example: 151 to 18
+                h_condition = (H >= h_range[0]) or (H <= h_range[1])
+            else:
+                h_condition = h_range[0] <= H <= h_range[1]
+        else:
+            h_condition = False
+
+        return h_condition and s_condition and v_condition
+
+    # Perform region growing
+    while queue:
+        i, j = queue.popleft()
+        # For 8 Neighbors
+        for di, dj in [
+            (-1, -1),
+            (-1, 0),
+            (-1, 1),
+            (0, -1),
+            (0, 1),
+            (1, -1),
+            (1, 0),
+            (1, 1),
+        ]:
+            ni, nj = i + di, j + dj
+            # Check that the neighbor is within the image
+            if 0 <= ni < hsv_img.shape[0] and 0 <= nj < hsv_img.shape[1]:
+                # If the neighbor is not yet part of the growth region
+                if region_mask[ni, nj] == 0:
+                    H, S, V = hsv_img[ni, nj]
+                    if fulfill_condition(H, S, V):
+                        region_mask[ni, nj] = 255
+                        queue.append((ni, nj))
+
+    return region_mask
+
+
+def apply_morphology(
+    mask: ndarray, kernel_size: tuple = (2, 2), operation: int = cv.MORPH_CLOSE
+) -> ndarray:
+    """
+    Applies morphological operations to a binary mask.
+
+    Parameters:
+        mask (ndarray): Binary input mask.
+        kernel_size (tuple, optional): Size of the kernel for morphological operation.
+        operation (int, optional): OpenCV morphological operation. Defaults to cv.MORPH_CLOSE.
+
+    Returns:
+        ndarray: Binary mask after morphological operation.
+    """
+    kernel = np.ones(kernel_size, np.uint8)
+    return cv.morphologyEx(mask, operation, kernel)
+
+
+def resize_binary_mask(mask: ndarray, target_size: tuple = (512, 512)) -> ndarray:
+    """
+    Resizes a mask to the target size and ensures it's binary.
+
+    Parameters:
+        mask (ndarray): Binary input mask.
+        target_size (tuple, optional): Target size (width, height). Defaults to (512, 512).
+
+    Returns:
+        ndarray: Resized binary mask.
+    """
+    resized = cv.resize(mask, target_size, interpolation=cv.INTER_AREA)
+    return np.where(resized > 0, 255, 0).astype(np.uint8)
+
+
+def process_class_mask(
+    hsv_img: ndarray, class_param: dict, target_size: tuple = (512, 512)
+) -> ndarray:
+    """
+    Process a single class mask through the entire pipeline: extraction, filtering,
+    region growing, morphology, and resizing.
+
+    Parameters:
+        hsv_img (ndarray): Input image in HSV color space.
+        class_param (dict): Parameters for this class including color_limits, min_area, and growth_conditions.
+        target_size (tuple, optional): Target size for the output mask. Defaults to (512, 512).
+
+    Returns:
+        ndarray: Processed binary mask for this class.
+    """
+    # Extract parameters
+    lower_bound, upper_bound = class_param["color_limits"]
+    min_area = class_param["min_area"]
+    growth_conditions = class_param["growth_conditions"]
+    kernel_size = class_param.get("kernel_size", (2, 2))
+
+    # 1. Extract color mask
+    color_mask = extract_color_mask(hsv_img, lower_bound, upper_bound)
+
+    # 2. Filter contours with class-specific min_area
+    filtered_mask = filter_contours(color_mask, min_area)
+
+    # 3. Apply region growing with class-specific conditions
+    region_mask = apply_region_growing(filtered_mask, hsv_img, growth_conditions)
+
+    # 4. Apply morphological operations with class-specific kernel
+    morphed_mask = apply_morphology(region_mask, kernel_size)
+
+    # 5. Resize mask
+    resized_mask = resize_binary_mask(morphed_mask, target_size)
+
+    return resized_mask
+
+
+def image_segmentation(
+    filename: str, base_mask_folder: str, output_folder: str
+) -> None:
+    """
+    Segments an image based on class-specific parameters, applies tailored processing
+    for each class, and saves the resulting masks as a NumPy array.
 
     Parameters:
         filename (str): The name of the image file to be segmented, including its extension (e.g., "image1.jpg").
@@ -38,10 +244,11 @@ def image_segmentation(filename: str, base_mask_folder: str, output_folder: str)
     Procedure:
         1. Extracts the base name of the image (without extension) to locate the corresponding mask file.
         2. Loads the mask image and converts it from BGR to HSV color space.
-        3. Applies a series of color-based masks to the HSV image.
-        4. Concatenates all masks along the channel dimension.
-        5. Resizes the combined mask to the specified dimensions.
-        6. Saves the resized combined mask as a NumPy file (.npy) in the specified output folder.
+        3. For each class:
+            a. Processes the class-specific mask with tailored parameters.
+            b. Adds the processed mask to the collection.
+        4. Concatenates all processed masks along the channel dimension.
+        5. Saves the combined mask as a NumPy file (.npy) in the specified output folder.
 
     Returns:
         None. The function saves the combined mask directly to the output folder.
@@ -53,28 +260,20 @@ def image_segmentation(filename: str, base_mask_folder: str, output_folder: str)
     bgr_img: ndarray = cv.imread(mask_path, 1)
     hsv_img: ndarray = cv.cvtColor(bgr_img, cv.COLOR_BGR2HSV)
 
-    # Create masks for each color and concatenate them
-    masks: list[ndarray] = []
-    for lower_bound, upper_bound in color_limits:
-        mask: ndarray = cv.inRange(hsv_img, lower_bound, upper_bound)
-            # Pseudocódigo para refatoração:
-            # Binarização
-            # Morfologia (adicionar tamanho de kernel de acordo com a classe)
-            # Filtragem por Contornos
-            # Resize para 512x512 com Interpolação AREA
-            # Binarização (Verificar o threshold na segmentação)
+    # Create and process masks for each class with class-specific parameters
+    processed_masks: list[ndarray] = []
 
-        # Add new axis for each mask binary mask
-        mask = mask[..., np.newaxis]
-        masks.append(mask)
+    for class_param in class_parameters:
+        # Process the mask for this class with its specific parameters
+        mask = process_class_mask(hsv_img, class_param)
 
-    # Concatenate masks along the channel dimension Z
-    combined_mask: ndarray = np.concatenate(masks, axis=2)
+        # Add new axis for each binary mask
+        binary_mask = mask[..., np.newaxis]
+        processed_masks.append(binary_mask)
 
-    # Resize the combined mask after segmentation
-    # resized_combined_mask = image_resize(combined_mask, input_size=512)
+    # Concatenate processed masks along the channel dimension Z
+    combined_mask: ndarray = np.concatenate(processed_masks, axis=2)
 
-    # Save resized combined mask as a NumPy file
+    # Save combined mask as a NumPy file
     output_file: str = os.path.join(output_folder, f"{img_number}.npy")
-    # np.save(output_file, resized_combined_mask)
     np.save(output_file, combined_mask)
